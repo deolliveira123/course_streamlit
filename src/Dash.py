ad2d70563d2b6283fd09e78108da59ae2f849e0a
andre <deolliveira@gmail.com>
2023-09-23 19:38:24 -0300

    ajuste_inclusão

--- src/Dash.py
+++ src/Dash.py
@@ -17,10 +17,32 @@ def num_format(valor, prefixo = ''):
 st.title(' DASHBOARD DE VENDAS :shopping_trolley: ')
 
 url = 'https://labdados.com/produtos'
-response = requests.request('GET', url)
-dados = pd.DataFrame.from_dict(response.json())
 
+regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
+st.sidebar.title('Filtros')
+regiao = st.sidebar.selectbox('Regiao', regioes)
+
+if regiao == 'Brasil':
+    regiao = ''
+
+todos_anos = st.sidebar.checkbox('Todo o periodo', value=True)
+if todos_anos:
+    ano = ''
+else:
+    ano = st.sidebar.slider('Ano', 2020, 2023)
+
+query = {'regiao':regiao.lower(), 'ano':ano}
+
+response = requests.request('GET', url, params=query)
+
+dados = pd.DataFrame.from_dict(response.json())
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
+
+filtro_vendedores = st.sidebar.multiselect('Vendedores', dados['Vendedor'].unique())
+
+if filtro_vendedores:
+    dados = dados[dados['Vendedor'].isin(filtro_vendedores)]
+
 
 receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
 receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
@@ -36,7 +58,9 @@ dados_grop_2 = dados.drop_duplicates(subset= 'Local da compra')[['Local da compr
     merge(dados_grop, left_on= 'Local da compra', right_index= True).sort_values('Preço', ascending= False)
 
 
+### tabelas vendedores
 
+vendedores = pd.DataFrame(dados.groupby('Vendedor')['Preço'].agg(['sum', 'count']))
 
 ## visualizações do gráfico
 
@@ -79,7 +103,7 @@ fig_receitas_categorias = px.bar(receita_categorias,
 fig_receitas_categorias.update_layout(yaxis_title = 'Receita')
 
 
-aba1, aba2, aba3 = st.tabs(['Receita', 'Quantidade de Vwndas', 'Vendedores'])
+aba1, aba2, aba3 = st.tabs(['Receita', 'Quantidade de Vendas', 'Vendedores'])
 
 
 with aba1:
@@ -106,13 +130,33 @@ with aba2:
 
 
 with aba3:
-         
+    qtd_vendedores = st.number_input('Quantidade de vendedores', 2, 10, 5) 
+    
     col1, col2 = st.columns(2)
     with col1:
         st.metric("Receita", num_format(dados['Preço'].sum(), 'R$'))
+        fig_receita_vendedores = px.bar(
+                    vendedores[['sum']].sort_values('sum', ascending=False).head(qtd_vendedores),
+                    x='sum',
+                    y=vendedores[['sum']].sort_values(['sum'], ascending=False).head(qtd_vendedores).index,
+                    text_auto=True,
+                    title=f'Top {qtd_vendedores} vendedores (receita)'
+                    )
+
+        st.plotly_chart(fig_receita_vendedores)
 
     with col2:
         st.metric('Quantidade de Vendas ', num_format(dados.shape[0]))
+        fig_vendas_vendedores = px.bar( 
+            vendedores[['count']].sort_values('count', ascending=False).head(qtd_vendedores),
+            x='count',
+            y=vendedores[['count']].sort_values(['count'], ascending=False).head(qtd_vendedores).index,
+            text_auto=True,
+            title=f'Top {qtd_vendedores} vendedores (quantidade de vendas)'
+            )
+
+        st.plotly_chart(fig_vendas_vendedores)
+ 
  
 
 
